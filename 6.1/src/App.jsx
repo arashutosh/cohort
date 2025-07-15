78ec3252fcc73a90e664c78aac3db8710741d0b3
arashutosh <arashutosh01@gmail.com>
2025-07-15 12:39:27 +0530

    refactor: clean up header import and improve button functionality

--- 6.1/src/App.jsx
+++ 6.1/src/App.jsx
@@ -1,22 +1,35 @@
-import Header from "./header"
+// import Header from "./header"
+import React from 'react';
 import { useState } from "react";
+
 function App() {
   const [title, setTitle] = useState("my name is ashutosh");
-  
-  function updateTitle(){
-    setTitle("my name is "+ Math.random());
-  }
-
+function updateTitle(){
+  setTitle("my name is "+Math.random());
+}
   return (
-    <div>
-      {/* <button onClick={() => setCount(count+1)}>increment</button> */}
-      <button onClick={updateTitle} >click me</button>
-      {/* <h1>count: {count}</h1> */}
-      <h1>title</h1>
-      <Header title={title}/>
+      <div>
+      <button onClick={updateTitle}>click me to update</button>
+      <br />
+      <Header title={title} />
+      <Header title="First Header" />
       <Header title="Second Header" />
+      <Header title="Third Header" />
+      <Header title="Fourth Header" />
+      <Header title="Fifth Header" />
+      <Header title="Sixth Header" />
+      <Header title="Seventh Header" />
+      <Header title="Eighth Header" />
     </div>
   )
 }
 
+const Header = React.memo(function Header({title}){
+  return(
+    <>
+      {title}
+      <br />
+    </>
+  )
+});
 export default App;
