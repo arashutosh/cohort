d0155e43c187a083a185d70d86c34dd31659b2e9
arashutosh <arashutosh01@gmail.com>
2025-07-15 23:51:58 +0530

    Adds CardWrapper component
    Adds a CardWrapper component to encapsulate content within a styled border.
    
    Removes previous header implementation and state management logic for title updates to focus on demonstrating the CardWrapper.

--- 6.1/src/App.jsx
+++ 6.1/src/App.jsx
@@ -1,35 +1,22 @@
-// import Header from "./header"
-import React from 'react';
-import { useState } from "react";
-
 function App() {
-  const [title, setTitle] = useState("my name is ashutosh");
-function updateTitle(){
-  setTitle("my name is "+Math.random());
+  return <div>
+<CardWrapper> hii thereeee</CardWrapper>
+  </div>
 }
+
+//   function TextComponent(){
+//   return(
+//     <div>
+//       hii there...!!
+//     </div>
+//   )
+// }
+
+function CardWrapper({children}){
   return (
-      <div>
-      <button onClick={updateTitle}>click me to update</button>
-      <br />
-      <Header title={title} />
-      <Header title="First Header" />
-      <Header title="Second Header" />
-      <Header title="Third Header" />
-      <Header title="Fourth Header" />
-      <Header title="Fifth Header" />
-      <Header title="Sixth Header" />
-      <Header title="Seventh Header" />
-      <Header title="Eighth Header" />
+    <div style={{border: "2px solid black"}}>
+      {children}
     </div>
   )
 }
-
-const Header = React.memo(function Header({title}){
-  return(
-    <>
-      {title}
-      <br />
-    </>
-  )
-});
 export default App;
